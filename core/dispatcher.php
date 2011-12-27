8920fe5b2568d40bbdf855cfa2f8e2b45ee4c216
Adria Cidre <adria.cidre@gmail.com>
2011-12-27 19:36:47 +0100

    Some phpdoc added

--- core/dispatcher.php
+++ core/dispatcher.php
@@ -1,10 +1,14 @@
 <?php
-
+/**
+ * Core Dispatcher
+ * 
+ * @version 0.1 
+ * @author Adrià Cidre [adria.cidre@gmail.com]
+ */
 
 class Core_Dispatcher 
 {
 	protected $_routes = array();
-
 
 	/**
 	 * Starts all the dispatcher logic
@@ -16,13 +20,11 @@ class Core_Dispatcher
 		$route = $this->getRoute($_SERVER['SCRIPT_URL']);
 
 		if (isset($route) && $route != null) {
-
 			$controllerName = $route['controller'];
 			$actionName = $route['action'] . 'Action';
 
 			$controller = new $controllerName($route['action']);
 			$controller->{$actionName}();
-
 		}
 	}
 
@@ -40,6 +42,12 @@ class Core_Dispatcher
 		}
 	}
 
+
+	/**
+	 * Load routes from external file default 
+	 * is app/config/routes.json
+	 * @return array
+	 */
 	private function _loadRoutes()
 	{
 		return json_decode(file_get_contents(CONFIG_PATH . 'routes.json'), true);
