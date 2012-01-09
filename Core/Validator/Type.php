a64aa52002aeca5dfbb522b5e6c3b46ef519f712
Adria Cidre <adria.cidre@gmail.com>
2012-01-09 07:24:28 +0100

    Added core validators

---
+++ Core/Validator/Type.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Description of Type
+ *
+ * @author Adrià Cidre <adria.cidre@gmail.com>
+ */
+class Core_Validator_Type 
+{
+    /**
+     * Checks if the given value is a string
+     * @param mixed $arg
+     * @return boolean
+     */
+    final public static function isString($arg)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+    
+    
+    /**
+     * Checks if a given value is a float
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isFloat($arg)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+
+    
+    /**
+     * Checks if the given value is an integer
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isInteger($arg)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+    
+    
+    /**
+     * Checks if the given value is a boolean
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isBoolean($arg)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+
+
+    /**
+     * Checks if the given value is an array
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isArray($arg)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+
+
+    /**
+     * Checks if the given value is a multidimensional array
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isMatrix($arg)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+
+    
+    /**
+     * Checks if the given value is an object
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isObject($arg)
+    {
+        // TODO Add correct validations
+        return true;
+    }   
+}
---
+++ Core/Validator/Value.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * Description of Value
+ *
+ * @author Adrià Cidre <adria.cidre@gmail.com>
+ */
+class Core_Validator_Value 
+{
+    
+    /**
+     * Checks if a given value is empty
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isEmpty($value)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+
+    
+    /**
+     * Checks if a given value is an email
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isEmail($value)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+
+    
+    /**
+     * Checks if a given value is positive
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isPositive($value)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+
+
+    /**
+     * Checks if a given value is great than zero
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isGreatThanZero($value)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+
+    
+    /**
+     * Checks if a given value is Zero
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isZero($value)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+
+    
+    /**
+     * Checks if a given value is a valid timestamp
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isTimestamp($value)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+
+    
+    /**
+     * Checks if a given value is an url
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isURL($value)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+
+    
+    /**
+     * Checks if a given value is a valid IP address
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isIP($value)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+
+    
+    /**
+     * Checks if a given value is a IPv4 address
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isIPv4($value)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+
+    
+    /**
+     * Checks if a given value is a valid IPv6 address
+     * @param mixed $arg
+     * @return boolean 
+     */
+    final public static function isIPv6($value)
+    {
+        // TODO Add correct validations
+        return true;
+    }
+    
+}
