636961f1d9982b6d246629c0eb8a8cab24bb2f9f
Kpacha <kpacha666@gmail.com>
2012-01-14 01:10:12 +0100

    example of the new features of the controller related to the management of the headers and content of the response

--- App/Controller/Default.php
+++ App/Controller/Default.php
@@ -16,4 +16,16 @@ class App_Controller_Default extends Core_Controller
         $this->_getView()->setParam('title', 'Hello world Title');
     }
 
+    public function jsonTestAction()
+    {
+        $this->addHeader('Content-type: application/json');
+        $mock = new stdClass();
+        $mock->prop1 = 1;
+        $mock->prop2 = 'demo';
+        $mock->prop3 = array('impressive', 'awesome', 'crazy', 'easy');
+        $mock->prop4 = 9.9;
+        $mock->check = "ñandú complain well /escaped. Very easy, isn't it?";
+        $this->_response = json_encode($mock);
+    }
+
 }
