1c27f8f27a1524bbab7265281ad0a0f9c561f4e7
Kpacha <kpacha666@gmail.com>
2012-01-30 01:11:23 +0100

    Fixing the notices and warnings throwed by the pqp library

--- library/Pqp/PhpQuickProfiler.php
+++ library/Pqp/PhpQuickProfiler.php
@@ -9,6 +9,18 @@ require_once __DIR__ . '/pqp/classes/PhpQuickProfiler.php';
 
 class Pqp_PhpQuickProfiler extends PhpQuickProfiler
 {
+
+    public function __construct($startTime, $config = '/pqp/')
+    {
+        $GLOBALS['debugger_logs'] = array(
+            'logCount' => 0,
+            'memoryCount' => 0,
+            'errorCount' => 0,
+            'speedCount' => 0
+        );
+        parent::__construct($startTime, $config);
+    }
+
 }
 
 ?>
